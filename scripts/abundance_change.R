48ffa396ce39f3c803cb6a637d22bff98abb435b
cour10eygrace <courtneygrace125@gmail.com>
2021-11-20 11:16:00 -0800

    clean up script-remove old plots

--- scripts/abundance_change.R
+++ scripts/abundance_change.R
@@ -24,6 +24,7 @@ plotcol<-c("#89C5DA", "#DA5724", "#74D944", "#CE50CA", "#3F4921", "#C0717C", "#C
            "#673770", "#D3D93E", "#38333E", "#508578", "#D7C1B1", "#689030", "#AD6F3B", "#CD9BCD", 
            "#D14285", "#6DDE88", "#652926", "#7FDCC0", "#C84248", "#8569D5", "#5E738F", "#D1A33D", 
            "#8A7C64", "#599861")
+color2<-c("#A98FBA", "#AA865A",  "#74870D", "#1C829D", "#3F4921")
 
 #NWT species counts----
 #all years-QA/QC JGS 12/18/20
@@ -49,11 +50,6 @@ spp_abund <- left_join(spp_abund, select(sppcomp, plot,block, code,snow, N, temp
   distinct(.) %>%
   unite(plotyear, plot, year, remove = F)
 
-#from gjamTimeXXX_dom.R
-rare<-subset(sppcomp, code=="XXX")%>%group_by(spp)%>%count()
-rare<-mutate(rare, plotyears_perc=n/720)
-#top 8 spp are in >85% of plot years, remaining spp are in less than half (max=47%)
-
 #look at total abundance in control plots over time to decide dominance categories 
 #calculate average hits per plot 
 #remove species with less than 20 total hits (observations) across time period 
@@ -62,14 +58,13 @@ rarectl<-subset(sppcomp, code=="XXX")%>%group_by(spp)%>%
 
 #plot raw cover over time 
 #break up into abundance groups 
-
 spp_abund<-filter(spp_abund, spp %in% rarectl$spp)%>%# filter out rare spp (<20 hits)
   mutate(group=case_when(spp=="DESCAE"~"DOM",
                                              spp=="ARTSCO"|spp=="GEUROS"|spp=="CARSCO"~"SUBDOM", 
                                              spp=="CALLEP"|spp=="BISBIS"|spp=="TRIPAR"|spp=="GENALG"~"MODERATE", 
                                              TRUE~"RARE"))
 spp_abundx<-subset(spp_abund,code!="XNX"&code!="PNX"& code!="PXX")
-unique(spp_abundx$spp)
+unique(spp_abundx$spp) #20 spp
 
 ggplot(data=spp_abund, aes(x = year, y = spp_hits, color = group)) +
   geom_point() +
@@ -79,26 +74,16 @@ ggplot(data=spp_abund, aes(x = year, y = spp_hits, color = group)) +
   facet_wrap(~ code) +theme_bw() +  scale_color_manual(values=plotcol)
 
 
-ggplot(data=spp_abund, aes(x = as.factor(year), y = spp_hits, color = group)) +
-  geom_boxplot() +
-  #geom_line()+
-  #geom_smooth(method = "lm", se = FALSE) +
-  #geom_smooth(method = "loess", se = FALSE) +
-  facet_wrap(~ code) +theme_bw() +  scale_color_manual(values=plotcol)
-
 #Were rare spp lost/gained over time? 
 rarespp<<-subset(spp_abund, group=="RARE")
 
-#spp w decent coverage
-rarespp<- mutate(rarespp, vrare=if_else(spp_hits>3, 1,0))%>%group_by(spp)%>%mutate(vrare=sum(vrare))
-                                        
-ggplot(data=subset(rarespp,code!="XNX"&code!="PNX"& code!="PXX"&vrare>0&spp!="JUN_SP"), aes(x = year, y = spp_hits, color = code)) +
+ggplot(data=subset(rarespp,code!="XNX"&code!="PNX"& code!="PXX"), aes(x = year, y = spp_hits, color = code)) +
   geom_point() +
   #geom_line()+
-  #geom_smooth(method = "lm", se = FALSE) +
   geom_smooth(method = "lm", se = FALSE) +
-  facet_wrap(~ spp) +theme_bw() +  scale_color_manual(values=plotcol)+ylab("hits")
-
+ # geom_smooth( method="lm", se = FALSE) +
+  facet_wrap(~ spp, scales="free") +theme_bw() +  scale_color_manual(values=plotcol)+ylab("hits")
+#some species do blink in and out of some treatments 
 
 #FIG S2-PLOT RAW HITS OF ALL SPP IN GJAMTIME MODEL 
 #all spp 
@@ -111,8 +96,7 @@ ggplot(data=subset(spp_abund,code!="XNX"&code!="PNX"& code!="PXX"), aes(x = year
   geom_smooth(method = "lm", se = F) +
   facet_wrap(group~spp) +theme_bw() +  scale_color_manual(values=plotcol)
 
-
-
+#BY SPECIES
 #CODYN change over time 
 #wrt 2006 for each time point 
 spp_change<-abundance_change(df = spp_abund,
@@ -128,6 +112,7 @@ spp_change<-mutate(spp_change, group=case_when(spp=="DESCAE"~"DOM",
                                              spp=="ARTSCO"|spp=="GEUROS"|spp=="CARSCO"~"SUBDOM", 
                                              spp=="CALLEP"|spp=="BISBIS"|spp=="TRIPAR"|spp=="GENALG"~"MODERATE", 
                                              TRUE~"RARE"))
+spp_change<-subset(spp_change, code!="XNX"&code!="PNX"& code!="PXX")
 
 #plot
 ggplot(data=spp_change, aes(x =year2, y=change, color=group))+
@@ -179,7 +164,7 @@ group_change<-mutate(group_change, years=year2-2006)
 delta_abund<-lm(change~0+ group:years:code,group_change)
 summary(delta_abund)
 
-#singularity with random effect of plot-level the change is calculated at
+#singularity with random effect of plot-but results the same 
 #group_change<-subset(group_change, code!="XNX"&code!="PNX"& code!="PXX") #runs singular fit but results same
 delta_abundx<-lmer(change~0+ group:years:code +(1|plot), group_change, 
 control = lmerControl(optimizer= "optimx", optCtrl  = list(method="nlminb")))
@@ -195,6 +180,7 @@ summary(delta_abundx)
 coeffs<-summary(delta_abundx)
 coeffs<-as.data.frame(coeffs$coefficients)
 coeffs$grouptreat<-row.names(coeffs)
+#write.csv(coeffs, "table1.csv")#Table 1
 
 coeffs<-separate(coeffs, grouptreat, c('group', 'x', 'treat'))%>%
   rename(pval=`Pr(>|t|)`)%>%select(-x)
@@ -211,13 +197,6 @@ coeffs$SE<-coeffs$`Std. Error`
 ggplot(data=coeffs, aes(x =Estimate, y=treat, color=group, shape=Response))+
   geom_point(stat="identity", position="identity", size=3) + theme_classic() +
   #geom_pointrange(aes(xmin = Estimate-SE, xmax = Estimate+SE))+
-  geom_vline(xintercept = 0, lty=2)+ xlab("Slope change in cover")+ 
-  scale_color_manual(values=plotcol)+xlim(-2, 2.5)
-
-ggplot(data=subset(coeffs,code!="XNX"&code!="PNX"& code!="PXX"),
-       aes(x =Estimate, y=treat, color=group))+
-  geom_point(stat="identity", position="identity", size=2) + theme_classic() +
-  geom_pointrange(aes(xmin = Estimate-SE, xmax = Estimate+SE))+
   geom_vline(xintercept = 0, lty=2)+ xlab("Slope change in cover")+ 
   scale_color_manual(values=plotcol)+xlim(-2, 2.5)
 
@@ -279,7 +258,6 @@ ggplot(data=group_changex, aes(x =year2, y=change, color=group))+
   ylab("cover change from year 1")+ xlab("year")+
   facet_wrap(~ code) +theme_bw()+ scale_color_manual(values=plotcol)
 
-#now plot for treatments with warming only 
 ggplot(data=subset(group_changex,code!="XNX"&code!="PNX"& code!="PXX"),
                    aes(x =year2, y=change, color=group))+
   geom_line(aes(y=fit))+
@@ -339,9 +317,6 @@ ggplot(data=newdat, aes(x =year2, y=change, color=group))+
   facet_wrap(~ code) +theme_bw()+ scale_color_manual(values=plotcol)
 
 #with fixed & random effects uncertainty 
-#WHY DOES THIS LOOK THE SAME AS THE ONE ABOVE?? SCALING ISSUE?
-#most of the differences in thi/lo and phi/lo are not for the dominant species so 
-#it it hard to see in the plots 
 ggplot(data=newdat, aes(x =year2, y=change, color=group))+
   geom_line(aes(y=distance))+
   geom_ribbon(aes(ymin = tlo, ymax = thi, color= group,), alpha = .15) +
@@ -369,24 +344,6 @@ ggplot(data=newdat, aes(x =year2, y=change, color=group))+
   ylab("cover change from year 1")+ xlab("year")+
   facet_wrap(~ code, scales = "free") +theme_bw()+ scale_color_manual(values=plotcol)
 
-#try without points-can see slight changes
-ggplot(data=newdat, aes(x =year2, y=change, color=group))+
-  geom_line(aes(y=distance))+
-  geom_ribbon( aes(ymin = plo, ymax =  phi, color= group), alpha = .15) +
-  #geom_point(aes(x =year2, y=change, color=group), alpha=0.5) + theme_classic()+  
-  #geom_jitter(aes(x =year2, y=change, color=group)) + theme_classic()+  
-  ylab("cover change from year 1")+ xlab("year")+
-  facet_wrap(~ code, scales = "free") +theme_bw()+ scale_color_manual(values=plotcol)
-
-#with fixed & random effects uncertainty 
-ggplot(data=newdat, aes(x =year2, y=change, color=group))+
-  geom_line(aes(y=distance))+
-  geom_ribbon(aes(ymin = tlo, ymax = thi, color= group,), alpha = .15) +
-  #geom_point(aes(x =year2, y=change, color=group), alpha=0.5) + theme_classic()+  
-  #geom_jitter(aes(x =year2, y=change, color=group)) + theme_classic()+  
-  ylab("cover change from year 1")+ xlab("year")+
-  facet_wrap(~ code, scales = "free") +theme_bw()+ scale_color_manual(values=plotcol)
-
 
 ###Fig 1
 newdat<-mutate(newdat, group = factor(group, 
@@ -395,8 +352,8 @@ newdat<-mutate(newdat, group = factor(group,
 ggplot(data=subset(newdat, code!="XNX"&code!="PNX"& code!="PXX"), aes(x =year2, y=change, color=group))+
   geom_line(aes(y=distance))+
   geom_ribbon( aes(ymin = plo, ymax =  phi, fill= group), alpha = .25, colour=NA) +
-  geom_point(aes(x =year2, y=change, color=group), alpha=0.5) + theme_classic()+  
+  geom_point(aes(x =year2, y=change, color=group), alpha=0.75) + theme_classic()+  
   #geom_jitter(aes(x =year2, y=change, color=group)) + theme_classic()+  
   ylab("Cover change from pre-treatment")+ xlab("year")+
-  facet_wrap(~ code) +theme_bw()+ scale_color_manual(values=plotcol)+ scale_fill_manual(values=plotcol)
+  facet_wrap(~ code) +theme_bw()+ scale_color_manual(values=color2)+ scale_fill_manual(values=color2)
 
