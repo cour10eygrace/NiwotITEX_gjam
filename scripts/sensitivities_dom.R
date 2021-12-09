b2fb9dffce3f966893783132117fc0ce76d66e93
cour10eygrace <courtneygrace125@gmail.com>
2021-12-09 14:06:07 -0800

    pull out eigenvalues

--- scripts/sensitivities_dom.R
+++ scripts/sensitivities_dom.R
@@ -31,15 +31,32 @@ asens4$treat<-'PNW'
 
 rhosens<-rbind(rhosens, rhosens1, rhosens2, rhosens3, rhosens4)
 rhosens<-rename(rhosens, rho_est=Estimate, rho_se=SE)
-rhosens$class<-row.names(rhosensx)
+rhosens$group<-row.names(rhosens)
 
 asens<-rbind(asens, asens1, asens2, asens3, asens4)
 asens<-rename(asens, alpha_est=Estimate, alpha_se=SE)
-asens$class<-row.names(asens)
+asens$group<-row.names(asens)
 
-sens_table<-left_join(asens, rhosens)
-
-
-modDAtimePNW$parameters$sensRho
+sens_table<-left_join(asens, rhosens)%>%select(-rho_se, -alpha_se)%>%select(group, treat, rho_est, alpha_est)
+#write.csv(sens_table, "tables/table2.csv")
 
 
+#Stabilty
+
+eigxxx<-modDAtimeXXX$parameters$alphaEigen%>%as.data.frame()
+eigxxw<-modDAtimeXXW$parameters$alphaEigen %>%as.data.frame()
+eigxnw<-modDAtimeXNW$parameters$alphaEigen %>%as.data.frame()
+eigpxw<-modDAtimePXW$parameters$alphaEigen %>%as.data.frame()
+eigpnw<-modDAtimePNW$parameters$alphaEigen %>%as.data.frame()
+
+eigxxx$treat<-"XXX"
+eigxxw$treat<-"XXW"
+eigxnw$treat<-"XNW"
+eigpxw$treat<-"PXW"
+eigpnw$treat<-"PNW"
+
+
+eigs<-rbind(eigxxx, eigxxw, eigxnw, eigpxw, eigpnw)
+eigs<-separate(eigs, col = ., into = c("real", 'imaginary'), sep =  "([+i])")
+
+write.csv(eigs, "tables/eigenvalues.csv")
